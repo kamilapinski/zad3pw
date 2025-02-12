81a709149abee91067dc3e616542b6303e7d63e0
Kamil Łapiński <kamill2004@gmail.com>
2025-02-12 23:23:07 +0100

    join_test works, select maybe works, then_test valgrind failed

--- src/future_combinators.c
+++ src/future_combinators.c
@@ -5,8 +5,6 @@
 #include "waker.h"
 #include "executor.h"
 
-// TODO: delete this once not needed.
-#define UNIMPLEMENTED (exit(42))
 
 static FutureState then_progress(Future* base, Mio* mio, Waker waker)
 {
@@ -79,7 +77,8 @@ ThenFuture future_then(Future* fut1, Future* fut2)
 typedef struct {
     Future base;
     Future* fut;
-    bool completed;
+    int nr;
+    JoinFuture* father;
 } JoinPackFuture;
 
 static FutureState join_pack_progress(Future* base, Mio* mio, Waker waker)
@@ -90,21 +89,33 @@ static FutureState join_pack_progress(Future* base, Mio* mio, Waker waker)
     FutureState fut_state = self->fut->progress(self->fut, mio, waker);
     
     if (fut_state == FUTURE_COMPLETED || fut_state == FUTURE_FAILURE) {
-        self->completed = true;
+        self->base.errcode = self->fut->errcode;
+        self->base.ok = self->fut->ok;
+
+        if (self->nr == 1)
+            self->father->fut1_completed = true;
+        else
+            self->father->fut2_completed = true;
+
+        if (self->father->fut1_completed && self->father->fut2_completed) {
+            // trzeba obudzić ojca
+            executor_spawn(waker.executor, (Future*)self->father);
+        }
+        
         return fut_state;
     }
     else {
-        waker_wake(&waker);
         return FUTURE_PENDING;
     }
 }
 
-JoinPackFuture future_join_pack(Future* fut)
+JoinPackFuture future_join_pack(Future* fut, int nr, JoinFuture* father)
 {
     return (JoinPackFuture) {
         .base = future_create(join_pack_progress),
         .fut = fut,
-        .completed = false,
+        .nr = nr,
+        .father = father,
     };
 }
 
@@ -113,69 +124,82 @@ static FutureState join_progress(Future* base, Mio* mio, Waker waker)
     JoinFuture* self = (JoinFuture*)base;
     debug("JoinFuture %p progress\n", self);
 
-    JoinPackFuture* pack1 = (JoinPackFuture*)self->fut1;
-    JoinPackFuture* pack2 = (JoinPackFuture*)self->fut2;
-
-    if (!self->fut1_completed && pack1->completed) {
-        self->fut1_completed = true;
-    }
-    if (!self->fut2_completed && pack2->completed) {
-        self->fut2_completed = true;
-    }
-
-    if (self->fut1_completed && self->fut2_completed) {
-        self->result.fut1.errcode = pack1->fut->errcode;
-        self->result.fut2.errcode = pack2->fut->errcode;
-        self->result.fut1.ok = pack1->fut->ok;
-        self->result.fut2.ok = pack2->fut->ok;
-
-        free(pack1);
-        self->fut1 = NULL;
-        free(pack2);
-        self->fut2 = NULL;
-
-        if (self->result.fut1.errcode || self->result.fut2.errcode) {
+    if (!self->fut1) {
+        JoinPackFuture* fut1 = malloc(sizeof(JoinPackFuture));
+        if (fut1 == NULL) {
+            debug("JoinPackFuture malloc failed\n");
             return FUTURE_FAILURE;
         }
-        return FUTURE_COMPLETED;
+
+        *fut1 = future_join_pack(self->result.fut1.ok, 1, self);
+
+        self->result.fut1.ok = NULL;
+
+        self->fut1 = (Future*)fut1;
     }
 
-    if (self->fut1 && !self->fut1->is_active)
-        executor_spawn(waker.executor, self->fut1);
-    if (self->fut2 && !self->fut1->is_active)
-        executor_spawn(waker.executor, self->fut2);
+    if (!self->fut2) {
+        JoinPackFuture* fut2 = malloc(sizeof(JoinPackFuture));
+        if (fut2 == NULL) {
+            free(self->fut1);
+            self->fut1 = NULL;
+            debug("JoinPackFuture malloc failed\n");
+            return FUTURE_FAILURE;
+        }
 
-    waker_wake(&waker);
+        *fut2 = future_join_pack(self->result.fut2.ok, 2, self);
+
+        self->result.fut2.ok = NULL;
+
+        self->fut2 = (Future*)fut2;
+    }
+
+    if (self->fut1 && self->fut2) {
+        if (self->fut1_completed && self->fut2_completed) {
+            self->result.fut1.ok = self->fut1->ok;
+            self->result.fut2.ok = self->fut2->ok;
+            self->result.fut1.errcode = self->fut1->errcode;
+            self->result.fut2.errcode = self->fut2->errcode;
+
+            JoinPackFuture* pack1 = (JoinPackFuture*)self->fut1;
+            JoinPackFuture* pack2 = (JoinPackFuture*)self->fut2;
+
+            self->fut1 = pack1->fut;
+            self->fut2 = pack2->fut;
+
+            free(pack1);
+            free(pack2);
+
+            if (self->result.fut1.errcode || self->result.fut2.errcode) {
+                return FUTURE_FAILURE;
+            }
+            else {
+                return FUTURE_COMPLETED;
+            }
+        }
+
+        if (!self->fut1->is_active)
+            executor_spawn(waker.executor, self->fut1);
+        
+        if (!self->fut2->is_active)
+            executor_spawn(waker.executor, self->fut2);
+
+    }
+
     return FUTURE_PENDING;
 }
 
 JoinFuture future_join(Future* fut1, Future* fut2)
 {
-    JoinPackFuture* pack1 = malloc(sizeof(JoinPackFuture));
-    if (pack1 == NULL) {
-        debug("PackFuture malloc failed\n");
-        return (JoinFuture){};
-    }
-
-    JoinPackFuture* pack2 = malloc(sizeof(JoinPackFuture));
-    if (pack2 == NULL) {
-        debug("PackFuture malloc failed\n");
-        free(pack1);
-        return (JoinFuture){};
-    }
-
-    *pack1 = future_join_pack(fut1);
-    *pack2 = future_join_pack(fut2);
-
     return (JoinFuture) {
         .base = future_create(join_progress),
-        .fut1 = (Future*)pack1,
-        .fut2 = (Future*)pack2,
+        .fut1 = fut1,
+        .fut2 = fut2,
         .fut1_completed = FUTURE_PENDING,
         .fut2_completed = FUTURE_PENDING,
         .result = {
-            .fut1 = { .errcode = 0, .ok = NULL },
-            .fut2 = { .errcode = 0, .ok = NULL },
+            .fut1 = { .errcode = 0, .ok = fut1 },
+            .fut2 = { .errcode = 0, .ok = fut1 },
         },
     };
 }
@@ -184,49 +208,68 @@ typedef struct {
     Future base;
     Future* fut;
     int nr;
-    bool finished;
-    bool stop;
-    int* which_finished;
+    SelectFuture* father;
 } SelectPackFuture;
 
 static FutureState select_pack_progress(Future* base, Mio* mio, Waker waker) {
-    UNIMPLEMENTED;
 
     SelectPackFuture* self = (SelectPackFuture*)base;
     debug("SelectPackFuture %p progress\n", self);
 
-    if (self->stop) {
-        if (self->which_finished == -1) {
-            free(self);
-        }
-        else {
-            self->which_finished = -1;
-        }
-        return FUTURE_COMPLETED;
+    if (self->father->which_completed == SELECT_COMPLETED_FUT1 ||
+    self->father->which_completed == SELECT_COMPLETED_FUT2 ||
+    self->father->which_completed == SELECT_FAILED_BOTH) {
+        return FUTURE_FAILURE;
     }
 
     FutureState fut_state = self->fut->progress(self->fut, mio, waker);
 
-    if (fut_state != FUTURE_PENDING) {
-        self->finished = true;
-        if (self->which_finished == 0) {
-            self->which_finished = self->nr;
+    if (fut_state == FUTURE_COMPLETED) {
+        self->base.ok = self->fut->ok;
+        self->base.errcode = self->fut->errcode;
+
+        if (self->nr == 1 && self->father->which_completed != SELECT_COMPLETED_FUT2) {
+            self->father->which_completed = SELECT_COMPLETED_FUT1;
+            self->father->base.progress((Future*)self->father, mio, waker);
         }
-        return fut_state;
+        else if (self->father->which_completed != SELECT_COMPLETED_FUT1) {
+            self->father->which_completed = SELECT_COMPLETED_FUT2;
+            self->father->base.progress((Future*)self->father, mio, waker);
+        }
+        return FUTURE_COMPLETED;
+    }
+    else if (fut_state == FUTURE_FAILURE) {
+        self->base.ok = self->fut->ok;
+        self->base.errcode = self->fut->errcode;
+
+        if (self->nr == 1) {
+            if (self->father->which_completed == SELECT_COMPLETED_NONE)
+                self->father->which_completed = SELECT_FAILED_FUT1;
+            else {
+                self->father->which_completed = SELECT_FAILED_BOTH;
+                self->father->base.progress((Future*)self->father, mio, waker);
+            }
+        }
+        else {
+            if (self->father->which_completed == SELECT_COMPLETED_NONE)
+                self->father->which_completed = SELECT_FAILED_FUT2;
+            else {
+                self->father->which_completed = SELECT_FAILED_BOTH;
+                self->father->base.progress((Future*)self->father, mio, waker);
+            }
+        }
+        return FUTURE_FAILURE;
     }
 
-    waker_wake(&waker);
     return FUTURE_PENDING;
 }
 
-SelectPackFuture future_select_pack(Future* fut, int nr, int* which_finished) {
+SelectPackFuture future_select_pack(Future* fut, int nr, SelectFuture* father) {
     return (SelectPackFuture) {
-        .base = feature_create(future_select_pack),
+        .base = future_create(select_pack_progress),
         .fut = fut,
         .nr = nr,
-        .finished = false,
-        .stop = false,
-        .which_finished = which_finished,
+        .father = father,
     };
 }
 
@@ -238,92 +281,45 @@ static FutureState select_progress(Future* base, Mio* mio, Waker waker)
     SelectPackFuture* pack1 = (SelectPackFuture*)self->fut1;
     SelectPackFuture* pack2 = (SelectPackFuture*)self->fut2;
 
-    if (!pack1 || !pack2) {
-        debug("Invalid progress");
-        return FUTURE_FAILURE;
-    }
-
-    FutureState fut1_state = FUTURE_PENDING;
-    FutureState fut2_state = FUTURE_PENDING;
-
-    if (pack1->finished) {
-        if (pack1->fut->errcode)
-            fut1_state = FUTURE_FAILURE;
-        else
-            fut1_state = FUTURE_COMPLETED;
-    }
-
-    if (pack2->finished) {
-        if (pack2->fut->errcode)
-            fut2_state = FUTURE_FAILURE;
-        else
-            fut2_state = FUTURE_COMPLETED;
-    }
-
-    int which_finished = pack1->which_finished;
-    if (fut1_state == FUTURE_COMPLETED && fut2_state == FUTURE_COMPLETED) {
-
-        // tutaj which_finished jest równe 1 lub 2
-        // o ile select_pack_progress dobrze działa
-
-        if (which_finished == 1) {
-            self->base.ok = pack1->fut->ok;
-            self->which_completed = SELECT_COMPLETED_FUT1;
-        }
-        else {
-            self->base.ok = pack2->fut->ok;
-            self->which_completed = SELECT_COMPLETED_FUT2;
-        }
-    }
-    else if (fut1_state == FUTURE_COMPLETED) {
-        self->base.ok = pack1->fut->ok;
-        self->which_completed = SELECT_COMPLETED_FUT1;
-    }
-    else if (fut2_state == FUTURE_COMPLETED) {
-        self->base.ok = pack2->fut->ok;
-        self->which_completed = SELECT_COMPLETED_FUT2;
-    }
-    else if (fut1_state == FUTURE_FAILURE && fut2_state == FUTURE_FAILURE) {
-        self->which_completed = SELECT_FAILED_BOTH;
-    }
-    else {
-        if (fut1_state == FUTURE_FAILURE) {
-            self->which_completed = SELECT_FAILED_FUT1;
-        }
-        else if (fut2_state == FUTURE_FAILURE) {
-            self->which_completed = SELECT_FAILED_FUT2;
-        }
-        else {
-            self->which_completed = SELECT_COMPLETED_NONE;
-        }
-
-        // jeżeli jedno zfailowało, to już naturalnie nie trzeba tego stopować - samo się zastopuje
-
-        waker_wake(&waker);
-        return FUTURE_PENDING;
-    }
-
-    // tutaj oznacza to, że już musimy wszystko zastopować
-    // TODO: deallokacja pack'ów musi nastąpić w ich progressie
-
-    pack1->stop = true;
-    pack2->stop = true;
-
-    if (!self->fut1->is_active)
+    if (pack1->father == NULL) {
+        pack1->father = self;
         executor_spawn(waker.executor, self->fut1);
-    
-    if (!self->fut2->is_active)
+    }
+
+    if (pack2->father == NULL) {
+        pack2->father = self;
         executor_spawn(waker.executor, self->fut2);
+    }
 
-    self->fut1 = NULL;
-    self->fut2 = NULL;
+    if (self->which_completed == SELECT_COMPLETED_FUT1) {
+        self->base.ok = self->fut1->ok;
+    }
 
-    if (self->which_completed == SELECT_COMPLETED_FUT1 || self->which_completed == SELECT_COMPLETED_FUT2) {
+    if (self->which_completed == SELECT_COMPLETED_FUT2) {
+        self->base.ok = self->fut2->ok;
+    }
+
+    if (self->which_completed == SELECT_COMPLETED_FUT1 ||
+    self->which_completed == SELECT_COMPLETED_FUT2) {
+        self->fut1 = pack1->fut;
+        self->fut2 = pack2->fut;
+
+        free(pack1);
+        free(pack2);
+
         return FUTURE_COMPLETED;
     }
-    else {
+    else if (self->which_completed == SELECT_FAILED_BOTH) {
+        self->fut1 = pack1->fut;
+        self->fut2 = pack2->fut;
+
+        free(pack1);
+        free(pack2);
+
         return FUTURE_FAILURE;
     }
+
+    return FUTURE_PENDING;
 }
 
 SelectFuture future_select(Future* fut1, Future* fut2)
@@ -341,25 +337,13 @@ SelectFuture future_select(Future* fut1, Future* fut2)
         return (SelectFuture){};
     }
 
-    int* which_finished = malloc(sizeof(int));
-    if (which_finished == NULL) {
-        debug("which_completed malloc failed\n");
-        free(pack1);
-        free(pack2);
-        return (SelectFuture){};
-    }
-
-    // 0 - nothing finished
-    // 1 - pack1 finished
-    // 2 - pack2 finished
-    *which_finished = 0;
-    *pack1 = future_select_pack(fut1, 1, which_finished);
-    *pack2 = future_select_pack(fut2, 2, which_finished);
+    *pack1 = future_select_pack(fut1, 1, NULL);
+    *pack2 = future_select_pack(fut2, 2, NULL);
 
     // TODO: zdeallocowac
 
     return (SelectFuture) {
-        .base = future_create(join_progress),
+        .base = future_create(select_progress),
         .fut1 = (Future*)pack1,
         .fut2 = (Future*)pack2,
         .which_completed = SELECT_COMPLETED_NONE,
